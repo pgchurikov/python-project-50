23e5bd485a3e0d63ea5cda376e5794746fb56b73
Pavel Churikov <p.g.churikov@gmail.com>
2024-08-11 17:11:20 +0300

    fix gendiff script still got None at the end

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,13 +2,10 @@
 
 
 from gendiff.gendiff_help import help
-from gendiff.build_diff import generate_diff
 
 
 def main():
     print(help())
-    path1, path2 = help.args
-    print(generate_diff(path1, path2))
 
 
 if __name__ == '__main__':
