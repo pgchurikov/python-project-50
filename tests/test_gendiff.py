83dfd0abb8782ed1c79464253f390672bf2b72f3
PCh <legocaesar@gmail.com>
2024-10-28 17:49:53 +0300

    fix test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,25 +2,57 @@ import pytest
 from gendiff.build_diff import generate_diff
 
 
-
-
 @pytest.mark.parametrize("path1, path2, style, expected", [
-    ('tests/fixtures/file1s.json', 'tests/fixtures/file2s.json', 'stylish', small_files),
-    ('tests/fixtures/file1s.yml', 'tests/fixtures/file2s.yml', 'stylish', small_files),
-    ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'stylish', big_files),
-    ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'stylish', big_files),
-    ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'plain', big_plain),
-    ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'plain', big_plain),
-    ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'json', big_json),
-    ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'json', big_json)
+    (
+        'tests/fixtures/file1s.json',
+        'tests/fixtures/file2s.json',
+        'stylish',
+        'tests/fixtures/small_files.txt'
+    ),
+    (
+        'tests/fixtures/file1s.yml',
+        'tests/fixtures/file2s.yml',
+        'stylish',
+        'tests/fixtures/small_files.txt'
+    ),
+    (
+        'tests/fixtures/file1.json',
+        'tests/fixtures/file2.json',
+        'stylish',
+        'tests/fixtures/big_files.txt'
+    ),
+    (
+        'tests/fixtures/file1.yml',
+        'tests/fixtures/file2.yml',
+        'stylish',
+        'tests/fixtures/big_files.txt'
+    ),
+    (
+        'tests/fixtures/file1.json',
+        'tests/fixtures/file2.json',
+        'plain',
+        'tests/fixtures/big_plain.txt'
+    ),
+    (
+        'tests/fixtures/file1.yml',
+        'tests/fixtures/file2.yml',
+        'plain',
+        'tests/fixtures/big_plain.txt'
+    ),
+    (
+        'tests/fixtures/file1.json',
+        'tests/fixtures/file2.json',
+        'json',
+        'tests/fixtures/big_json.txt'
+    ),
+    (
+        'tests/fixtures/file1.yml',
+        'tests/fixtures/file2.yml',
+        'json',
+        'tests/fixtures/big_json.txt'
+    )
 ])
 def test_all(path1, path2, style, expected):
-    with open('tests/fixtures/small_files.txt', 'r') as file:
-        small_files = file.read()
-    with open('tests/fixtures/big_files.txt', 'r') as file:
-        big_files = file.read()
-    with open('tests/fixtures/big_plain.txt', 'r') as file:
-        big_plain = file.read()
-    with open('tests/fixtures/big_json.txt', 'r') as file:
-        big_json = file.read()
+    with open(expected, 'r') as file:
+        expected = file.read()
     assert generate_diff(path1, path2, style) == expected
